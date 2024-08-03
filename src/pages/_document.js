58bb052d42b81f2b2a705f5fb74453fe51352856
nurngnarakorn <nurng.narakorn@gmail.com>
2024-08-03 15:55:54 +0700

    add document head
    add document head

---
+++ src/pages/_document.js
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2877713589134666"
+            crossOrigin="anonymous"
+            id="adsense-banner"
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
